b9ef646052bb20c0263557c6ea58795af41a7796
ketyung <ketyung@gmail.com>
2025-05-12 08:40:26 +0800

    Initial Commit

---
+++ SwiftUINavigationStack/SwiftUINavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationStackApp.swift
+//  SwiftUINavigationStack
+//
+//  Created by Chee Ket Yung on 12/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
